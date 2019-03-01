86b7ae35b5e4bbb0e740c1ef4510ef47b81818d6
Heba ElGalad <heba957@gmail.com>
2019-03-01 23:38:48 +0200

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+// function registerServiceWorker() {
+//   if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => {
+//       navigator.serviceWorker
+//         .register('/sw.js')
+//         .then(registration => {
+//           console.log('SW registered: ', registration);
+//         })
+//         .catch(registrationError => {
+//           console.log('SW registration failed: ', registrationError);
+//         });
+//     });
+//   }
+// }
+
+// Uncomment for service worker support
+// registerServiceWorker();
